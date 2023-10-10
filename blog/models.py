61736225802d4f1df32f1a9c69e3b73932ac16a3
Bryan Misa <bryan.misa@gmail.com>
2023-10-10 20:51:01 +0300

    Initialize

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,33 @@
 from django.db import models
 from django.utils import timezone   
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Post(models.Model):
 
+    # enum type, suggested to use enum classes for text choices
     class Status(models.TextChoices):
         DRAFT = 'DF', 'DRAFT'
         PUBLISHED = 'PB', 'PUBLISHED'
 
     title = models.CharField(max_length=250) 
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                                # models.CASCADE will delete all related post
+                                # when the user is deleted.
+                                on_delete=models.CASCADE,
+                                # allows us to access related objects from a user
+                                # by using the user.blog_posts notation
+                                related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=2, choices=Status.choices,default=Status.DRAFT)
+    status = models.CharField(max_length=2, 
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
         
